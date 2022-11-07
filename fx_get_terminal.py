c2618ac8419a227fc050a2867196da22021bfaa1
Portfedh <59422723+portfedh@users.noreply.github.com>
2022-11-07 06:53:26 -0800

    Refactor: Changed to OOP

--- fx_get_terminal.py
+++ fx_get_terminal.py
@@ -2,78 +2,70 @@
 # Para un periodo de fechas.
 # Autor: Pablo Cruz Lemini
 
-
-# Imports
-#########
 import os
 import requests
 import pandas as pd
 
-# Get the API Token
-####################
-api_token = os.environ.get("token_banxico")
-# api_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx'  # Alternativa
-
-
-# Funcion de descarga de datos
-##############################
-def descarga_banxico(serie, fechainicio, fechafin, token):
+class GetFxTerminal():
+    api_token = os.environ.get("token_banxico")
+    obligaciones = "SF60653"
     www = "https://www.banxico.org.mx/SieAPIRest/service/v1/series/"
-    url = (www
-           + serie
-           + "/datos/"
-           + fechainicio
-           + "/"
-           + fechafin
-           )
-    # Se crea un diccionarion con el token del API
-    headers = {"Bmx-Token": token}
-    # Hacer un GET request a la pagina del API
-    response = requests.get(url, headers=headers)
-    # Revisar el codigo de respuesta
-    status = response.status_code
-    # Si el estatus esta OK:
-    if status == 200:
-        # Crear un objeto json
-        raw_data = response.json()
-        # Accesar los datos dentro del objeto json
-        data = raw_data["bmx"]["series"][0]["datos"]
-        # Creamos un dataframe con los datos
-        df = pd.DataFrame(data)
-        # Transformamos los datos de strings a floats
-        df["dato"] = df["dato"].apply(lambda x: float(x))
-        # Transformamos las fechas de strings a datetime
-        df["fecha"] = pd.to_datetime(df["fecha"], format="%d/%m/%Y")
-        # Renombramos las columnas
-        df.columns = ['Fecha', 'Tipo de Cambio']
-        return(df)
-    # Si el estatus tiene error:
-    else:
-        # Imprimir el error
-        print(status)
+
+    def __init__(self):
+        self.get_dates()
+        self.get_data(
+            GetFxTerminal.obligaciones,
+            self.fecha_inicial,
+            self.fecha_final,
+            GetFxTerminal.api_token
+            )
+        self.print_output()
+
+    def get_dates(self):
+        print("\nBusqueda de FX para Solventar Obligaciones: \n")
+        self.fecha_inicial = str(input("Fecha Inicial de Busqueda yyyy-mm-dd: "))
+        self.fecha_final = str(input("Fecha Final de Busqueda yyyy-mm-dd: "))
+
+    def get_data(self, serie, fechainicio, fechafin, token):
+        url = (GetFxTerminal.www
+            + serie
+            + "/datos/"
+            + fechainicio
+            + "/"
+            + fechafin
+            )
+        # Se crea un diccionarion con el token del API
+        headers = {"Bmx-Token": token}
+        # Hacer un GET request a la pagina del API
+        response = requests.get(url, headers=headers)
+        # Revisar el codigo de respuesta
+        status = response.status_code
+        # Si el estatus esta OK:
+        if status == 200:
+            # Crear un objeto json
+            raw_data = response.json()
+            # Accesar los datos dentro del objeto json
+            data = raw_data["bmx"]["series"][0]["datos"]
+            # Creamos un dataframe con los datos
+            self.df = pd.DataFrame(data)
+            # Transformamos los datos de strings a floats
+            self.df["dato"] = self.df["dato"].apply(lambda x: float(x))
+            # Transformamos las fechas de strings a datetime
+            self.df["fecha"] = pd.to_datetime(self.df["fecha"], format="%d/%m/%Y")
+            # Renombramos las columnas
+            self.df.columns = ['Fecha', 'Tipo de Cambio']
+            return self.df
+        # Si el estatus tiene error:
+        else:
+            # Imprimir el error
+            print(status)
+
+    def print_output(self):
+        print("\n")
+        print(self.df.to_string(index=False))
+        print("\n")
 
 
 if __name__ == '__main__':
-
-    # Fechas para el calculo
-    ########################
-    print("\nBusqueda de FX para Solventar Obligaciones: \n")
-    fecha_inicial = input("Fecha Inicial de Busqueda yyyy-mm-dd: ")
-    fecha_final = input("Fecha Final de Busqueda yyyy-mm-dd: ")
-
-    # Determinando la serie: 'Para solventar Obligaciones'
-    ######################################################
-    obligaciones = "SF60653"
-
-    # Ejecutando the function
-    #########################
-    df = descarga_banxico(obligaciones,
-                          str(fecha_inicial),
-                          str(fecha_final),
-                          api_token)
-
-    # Mostrar los datos en la terminal
-    ##################################
-    print("\n")
-    print(df.to_string(index=False))
-    print("\n")
+    print('Hello Pablo')
+    oGetFx = GetFxTerminal()
